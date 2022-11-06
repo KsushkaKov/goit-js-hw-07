6a43a9fae3bf0944cae31d4e5b9e204645f61cc6
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-11-06 17:11:12 +0200

    Task 2 homework 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const elements = [];
+
+galleryItems.forEach((el) => {
+  const galleryItem = document.createElement("a");
+  galleryItem.className = "gallery__item";
+  galleryItem.href = el.original;
+  const galleryImg = document.createElement("img");
+  galleryImg.className = "gallery__image";
+  galleryImg.src = el.preview;
+  galleryImg.alt = el.description;
+  galleryImg.setAttribute("title", el.description);
+
+  galleryItem.append(galleryImg);
+  elements.push(galleryItem);
+});
+
+gallery.append(...elements);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
